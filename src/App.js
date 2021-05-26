cbd2d26179d2472654ce4a0f9c2dbaad20289f51
CaitlinGreer <clento1121@gmail.com>
2021-05-26 10:05:01 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component} from 'react';
+import { Route, Switch } from 'react-router-dom'
 
-function App() {
+import Nav from './Nav/Nav'
+import LandingPage from './LandingPage/LandingPage'
+import FindAMeal from './FindAMeal/FindAMeal'
+
+class App extends Component  {
+  
+  renderRoutes(){
+    return (
+      <div className='App'>
+        <Switch>       
+          <Route
+              exact
+              path='/'
+              component={LandingPage}
+          />
+          <Route
+              path='/find-meal'
+              component={FindAMeal}
+          />
+          {/* 
+          <Route
+              path='/jokes-list'
+              component={JokesList}
+          /> */}
+        </Switch>
+      </div>
+    );
+  }
+
+  render(){
   return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
+    <div>
+      <Nav />
+      <main className='main-container'>
+        
+        {this.renderRoutes()}
+      </main>      
+
+    </div>
+
   );
-}
+}}
 
 export default App;
