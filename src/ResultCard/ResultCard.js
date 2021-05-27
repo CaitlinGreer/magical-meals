e7b04830896c52aa8806cc72675be6e8ee7a9790
CaitlinGreer <clento1121@gmail.com>
2021-05-27 11:39:15 -0400

    fixed error

--- src/ResultCard/ResultCard.js
+++ src/ResultCard/ResultCard.js
@@ -2,28 +2,26 @@ import React from 'react'
 
 import './ResultCard.css'
 
-function ResultCard(props){
-        return (
-            <div>
-                <ul className='meal-card'>
-                    <li key={props.id}>
-                        <p>{props.restaurant}
-                        <br />
-                        <br />{props.meal_name}
-                        <br />{props.description}
-                        <br />{props.location}
-                        <br />{props.park}
-                        <br />{props.price}
-                        </p>
-                        
-                    </li>
-                </ul>
-                
-
-                
-            </div>  
-        )
-    }
+function ResultCard(props) {
+    return (
+        <div>
+            <div className='meal-card'>
+                <p>{props.restaurant}
+                    <br />
+                    <br />{props.meal_name}
+                    <br />{props.description}
+                    <br />{props.location}
+                    <br />{props.park}
+                    <br />{props.price}
+                    <br />
+                    <br />Vegan: {props.vegan}
+                    <br />Vegetarian: {props.vegetarian}
+                    <br />Gluten Free: {props.gluten_free}
+                </p>
+            </div>
+        </div>
+    )
+}
 
 
 export default ResultCard
