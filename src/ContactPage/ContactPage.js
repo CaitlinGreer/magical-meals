df0fce43a35fc7b24cf6a4a85a49e1de617d9805
CaitlinGreer <clento1121@gmail.com>
2021-06-09 10:48:43 -0400

    removed console.log

--- src/ContactPage/ContactPage.js
+++ src/ContactPage/ContactPage.js
@@ -6,18 +6,16 @@ import './ContactPage.css'
 class ContactPage extends Component {
   
     render() {
-        console.log('Contact!')
         return(
             <div className="contact-container">
                 <h1 className='page-heading'>Contact Us</h1>
                 <div className='contact-subinfo'>
                     <p>The menus at Walt Disney World are always changing.  So we may not always be up to date.                
-                    But that's where you come in...
+                    But that's where you can help add to the magic...
                     <br />
                     If you know of a meal we missed and want to share the magic, let us know about it!
                     <br />
-                    Click on the button below, and fill out the form with as much information as you can and we'll work on
-                    adding it to our options!     
+                    Click on the button below, and fill out the form with as much information as you can and we'll add it to our options!     
                 </p>
                 </div>
                 <div className='contact-form-link'>
