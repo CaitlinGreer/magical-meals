db4121683fc2c5cb3e5c39086fb3098712e11a76
CaitlinGreer <clento1121@gmail.com>
2021-06-15 15:49:24 -0400

    reformatted styling

--- src/LandingPage/Footer/Footer.js
+++ src/LandingPage/Footer/Footer.js
@@ -10,19 +10,22 @@ class Footer extends React.Component {
         return (
             <div >
                 <footer className='footer-container'>
-                <p>Created by Caitlin Greer</p>
-                    <ul>
-                        <li>
-                            <a href='https://github.com/CaitlinGreer'>
-                                <FontAwesomeIcon icon={faGithubSquare} className='github' />
-                            </a>
-                        </li>
-                        <li>
-                            <a href='https://www.linkedin.com/in/caitlingreer/'>
-                                <FontAwesomeIcon icon={faLinkedin} className='linked-in' />
-                            </a>
-                        </li>
-                    </ul>
+                    <p>Created by Caitlin Greer</p>
+                    <div className='links-container'>
+                        <ul className='footer-links'>
+                            <li>
+                                <a href='https://github.com/CaitlinGreer'>
+                                    <FontAwesomeIcon icon={faGithubSquare} className='github' />
+                                </a>
+                            </li>
+                            <li>
+                                <a href='https://www.linkedin.com/in/caitlingreer/'>
+                                    <FontAwesomeIcon icon={faLinkedin} className='linked-in' />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
                 </footer>
             </div>
         )
