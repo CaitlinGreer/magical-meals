a0db46fc12109cc16e685b6a1f7c438493ec592b
CaitlinGreer <clento1121@gmail.com>
2021-06-16 15:12:25 -0400

    added logo and down arrow

--- src/LandingPage/Header/Header.js
+++ src/LandingPage/Header/Header.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeaf, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
 import './Header.css'
 
 class Header extends Component {
     render() {
         return (
             <div className='header-container'>
-                <header>
-                    <h1>Magical Meals</h1>
-                    <h2>Find The Disney Meals You Love with the Ingredients Your Body Wants</h2>
+                <header className='container'>
+                    <h1 className='text1'>
+                        <span className='magical-header'>Magical</span> 
+                        <span className='magical-header2'>
+                            <FontAwesomeIcon icon={faLeaf} className='leaf-header' />
+                        <span className='meals-header'>MEALS</span>
+                        </span></h1>
+                    <h2 className='text2'>Here to Help You Find The Disney World Food You Love 
+                    <br />
+                    With The Ingredients Your Body Wants</h2>
                 </header>
-                
+                <a href='#main-page-container' className='arrow-icon'>
+                    <FontAwesomeIcon icon={faChevronDown}  />
+                </a>
             </div>
         )
     }
