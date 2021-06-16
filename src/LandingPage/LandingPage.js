b2ec93c2aea40cff52a689c7bc7b798364faa9b5
CaitlinGreer <clento1121@gmail.com>
2021-06-16 15:11:22 -0400

    reconfigured welcome message

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './LandingPage.css'
 
 import Header from './Header/Header'
@@ -8,28 +8,31 @@ import Footer from './Footer/Footer'
 class LandingPage extends Component {
     render() {
         return (
-            <div className='home-page-container'>
-                <Header />
+            <div>
+                <div className='home-page-container'>
+                    <Header />
 
-                <div className='main-page-container'  id='main-page-container'>
-                <div className='welcome-info'>
-                     <h3>Make finding your meals at Disney World easy</h3>
-                    <p>The last thing anyone wants to spend time doing on their vacation 
-                        is figuring out where to eat.  Especially when you've got certain dietary restrictions.
+                    <div className='main-page-container' id='main-page-container'>
+                        <div className='welcome-info'>
+                            <h3 className='info-heading'>Make finding your meals at Disney World easy</h3>
+                            <p>The last thing anyone wants to spend time doing on their vacation
+                            is figuring out where to eat.  Especially when you've got certain dietary restrictions.
                     </p>
-                    <p>Disney World is full of amazing food, but finding the right options for vegan, vegetarian or 
-                        gluten free diets can often times be difficult.
-                    </p>  
-                    <p>Magical Meals is here to help simplify that process and make it easy to find 
-                        the food you love without sacrificing any of the magic.
+                            <p>Disney World is full of amazing food, but finding the right options for vegan, vegetarian or
+                            gluten free diets can often times be difficult.
                     </p>
+                            <p>Magical Meals is here to help simplify that process and make it easy to find
+                        the food you love without sacrificing any of the magic.  Get started by clicking <Link to='/find-meal'>here</Link> or on Find A Meal above!
+                    </p>
+                        </div>
+
+                    </div>
                 </div>
-               
-                </div>
+
                 <footer>
-                    <Footer /> 
+                    <Footer />
                 </footer>
-               
+
             </div>
         )
     }
