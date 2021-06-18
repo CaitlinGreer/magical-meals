1d9d27dca1dae1fd8fe71b5d81a216e8c00822e1
CaitlinGreer <clento1121@gmail.com>
2021-06-18 16:07:44 -0400

    reconfigured code and styled

--- src/ResultCard/ResultCard.js
+++ src/ResultCard/ResultCard.js
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import RestrictionsIcons from '../RestrictionsIcons/RestrictionsIcons'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 import './ResultCard.css'
 
 function ResultCard(props) {
+    useEffect(() => {
+        Aos.init({});
+    })
+
     return (
         <div>
-            <div className='meal-card'>
-                <p>{props.restaurant_name}
-                    <br />
-                    <br />{props.meal_name}
-                    <br />{props.meal_description}
-                    <br />{props.meal_location}
-                    <br />${props.price}
-                    <br />
+            <div className='individual-card' data-aos='slide-right'>
+                <div className='location-restaurant-name'>
+                    <div className='location'>{props.meal_location}</div>
+                    <div className='restaurant-name'>{props.restaurant_name}</div>
+                </div>
+                <div className='mealname-price'>
+                    <div className='meal-name'>{props.meal_name}</div>
+                    <div className='price'>${props.price}</div>
+
+                   
+                </div>
+                <div className='meal-description'>
+                    <div className='description'>{props.meal_description}</div>
+                </div>
                     
-                </p>
-                <RestrictionsIcons 
+
+                <RestrictionsIcons
                     is_vegan={props.is_vegan}
                     is_vegetarian={props.is_vegetarian}
                     is_glutenfree={props.is_glutenfree}
                 />
             </div>
+
+
         </div>
+
     )
 }
 
