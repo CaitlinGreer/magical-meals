0239db05a02d3be9df78c8bfd1bb92904daec3ce
CaitlinGreer <clento1121@gmail.com>
2021-06-23 09:58:18 -0400

    added br between p

--- src/ContactPage/ContactPage.js
+++ src/ContactPage/ContactPage.js
@@ -11,15 +11,19 @@ class ContactPage extends Component {
                 <h1 className='page-heading'>Contact Us</h1>
                 <div className='contact-subinfo'>
                     <p>The menus at Walt Disney World are always changing.  So we may not always be up to date.                
+                    <br />
+                    <br />
                     But that's where you can help add to the magic...
                     <br />
+                    <br />
                     If you know of a meal we missed and want to share the magic, let us know about it!
+                    <br />
                     <br />
                     Click on the button below, and fill out the form with as much information as you can and we'll add it to our options!     
                 </p>
                 </div>
-                <div className='contact-form-link'>
-                    <Link to={'/contact-form'}>Share A Meal</Link>
+                <div >
+                  <button className='link-to-share'><Link to={'/contact-form'} className='share-link'>Click To Share</Link></button>  
                 </div>
             </div>
         )
